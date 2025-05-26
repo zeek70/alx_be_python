2ce48e2e5cc9c9f8fadc8439485b53b0ace5bf7e
zeek70 <zaxbaaba@gmail.com>
2025-05-26 19:22:07 +0100

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,5 @@
+number1 = 8
+number2 = 4
+print(f"Addition of {number1} and {number2}  is {number1 + number2}")
+print(f"Subtraction of {number1} and {number2}  is {number1 - number2}")
+print(f"Multiplication of {number1} and {number2}  is {number1 * number2}")
